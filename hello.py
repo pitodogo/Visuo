6ea99e253320280dd0e65842a72a2c5590c1e1dd
rootlesstree <pitodogo@Patricks-MacBook-Pro.local>
2015-08-27 19:41:19 +0800

    Add version 3 click mode

--- hello.py
+++ hello.py
@@ -2,9 +2,6 @@ from flask import Flask, jsonify, render_template, request
 import cPickle
 import json
 import random
-import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
 app = Flask(__name__)
 
 @app.route('/')
