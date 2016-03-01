ca79484c2952d6444cb02619e078aa10ab226acf
pitodogo <pitodogo@gmail.com>
2016-03-01 11:53:25 +0800

    Start to work on x3dom to pick up and drag object

---
+++ 3DVisuo/hello.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, render_template, request
+import cPickle
+import json
+import random
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index(methods=None):
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
+
